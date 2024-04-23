2f0eddc20254ab945db5743ca8eed1f0388a9a71
usuario <enzo.sanmartin@usach.cl>
2024-04-22 20:48:40 -0400

    se crea el proyecto

---
+++ AUTOFIX_backend/src/main/java/com/autofix/AUTOFIX_backend/AutofixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autofix.AUTOFIX_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofixBackendApplication.class, args);
+	}
+
+}
