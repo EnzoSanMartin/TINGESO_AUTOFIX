0534391c4046fa91ccb59beeae0c2845667498ca
usuario <enzo.sanmartin@usach.cl>
2024-04-22 21:02:34 -0400

    se creo la primera entidad

---
+++ AUTOFIX_backend/src/main/java/com/autofix/AUTOFIX_backend/entities/VehiculoEntity.java
@@ -0,0 +1,28 @@
+package com.autofix.AUTOFIX_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehiculo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class VehiculoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String patente;
+    private String marca;
+    private String modelo;
+    private String tipo;
+    private String tipoMotor;
+    private int añoFabricacion;
+    private int NAsientos;
+}
